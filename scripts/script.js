bd63196ba104fac476ec53a3b3cc740c1388ccf6
AlphaCharlieFoxtrot <albertini.fanomezantsoa@gmail.com>
2023-09-17 18:44:51 +0200

    correction script

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
-
-
+//the third.
 function verifierEmail(email) {
     let regex = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
     let resultat = regex.test(email)
@@ -7,25 +6,52 @@ function verifierEmail(email) {
     return resultat
 }
 
-function success() {
-    let email = document.getElementById("email").value
+function errorState() {
+    let errorEmail =    document.getElementById("email") 
+    
+    errorEmail.style.border = "2px solid orange"
+    errorEmail.style.backgroundColor = "rgba(255, 166, 0, 0.1)"
+    errorEmail.setAttribute("placeholder", "ash#loremcompany.com")
 
-    if (email === "") {
-        console.log("Veuillez entrer une adresse e-mail.")
-        return
-    }
+    document.querySelector(".opacityZero").style.opacity = "1"
+}
+
+function validateState(email){
+    let successBox = document.querySelector(".success-conteiner")
+    successBox.classList.add("active")
+    const emailSend = document.querySelector(".text-content span")
+    emailSend.textContent = email
+}
+
+
+function success() {
+    const baliseEmail = document.getElementById("email")
+    const email = baliseEmail.value
 
     let process = verifierEmail(email)
 
-    if (process === true){
-        let successBox = document.querySelector(".success-conteiner")
-        successBox.classList.add("active")
+    if (email === "") {
+        //console.log("Veuillez entrer une adresse e-mail.")
+        errorState()
+        return
+    }else if (process === true){
+        validateState(email)
     } else {
-        console.log("L'adresse e-mail n'est pas valide.")
+        //console.log("L'adresse e-mail n'est pas valide.")
+        errorState()
         document.getElementById("email").value = ''
     }
 }
 
-let btnEnvoyer = document.getElementById("submit")
-btnEnvoyer.addEventListener("click", success)
+function dismissPopUp(){
+    let successBox = document.querySelector(".success-conteiner")
+    successBox.classList.remove("active")
+}
 
+function newsletterSignUp(){
+    let btnDismiss = document.getElementById("dismiss")
+    btnDismiss.addEventListener("click", dismissPopUp)
+
+    let btnEnvoyer = document.getElementById("submit")
+    btnEnvoyer.addEventListener("click", success)
+}
